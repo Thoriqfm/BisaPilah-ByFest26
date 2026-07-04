7e0b70c90858b68de76e122117570b5e0a06c897
Thoriqfm <tfarrelm@gmail.com>
2026-07-04 20:30:30 +0700

    add aset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import SplashScreen from "../components/SplashScreen";
+import SplashScreen from "../components/Splash/SplashScreen";
+import HeroSection from "../components/Hero/HeroSection";
 
 export default function Home() {
     const [splashDone, setSplashDone] = useState(false);
@@ -12,10 +13,17 @@ export default function Home() {
             <SplashScreen onComplete={() => setSplashDone(true)} />
             {/* )} */}
 
-            <main className="flex min-h-screen items-center justify-center">
-                <p className="text-xl text-gray-600">
-                    Home page coming soon...
-                </p>
+            <main>
+                <HeroSection />
+                {/* Game section — nanti */}
+                <section
+                    id="game-section"
+                    className="min-h-screen bg-white flex items-center justify-center"
+                >
+                    <p className="text-gray-400 text-xl">
+                        Game section coming soon...
+                    </p>
+                </section>
             </main>
         </>
     );
