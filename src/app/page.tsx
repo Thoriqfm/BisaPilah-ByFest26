bab23e5fb2edd7c886d485f09181aa6a71193e3b
Thoriqfm <tfarrelm@gmail.com>
2026-07-06 19:18:44 +0700

    fix: fixing games

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import SplashScreen from "../components/ui/SplashScreen";
 import HeroSection from "../components/sections/HeroSection";
 import GameSection from "../components/sections/GameSection";
+import ChoiceSection from "@/components/sections/ChoiceSection";
 export default function Home() {
     const [splashDone, setSplashDone] = useState(false);
 
@@ -16,6 +17,7 @@ export default function Home() {
             <main className="overflow-x-hidden">
                 <HeroSection />
                 <GameSection />
+                <ChoiceSection />
             </main>
         </>
     );
